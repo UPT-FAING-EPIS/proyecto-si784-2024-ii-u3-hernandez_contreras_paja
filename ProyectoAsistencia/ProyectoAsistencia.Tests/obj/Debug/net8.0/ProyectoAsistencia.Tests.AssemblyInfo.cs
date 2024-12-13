4e029f2ad6e7368b854a18e52ea552e55d1a14d9
MaynerAC <ma2020067145@virtual.upt.pe>
2024-12-12 21:16:01 -0500

    reporte de integrancion

--- ProyectoAsistencia/ProyectoAsistencia.Tests/obj/Debug/net8.0/ProyectoAsistencia.Tests.AssemblyInfo.cs
+++ ProyectoAsistencia/ProyectoAsistencia.Tests/obj/Debug/net8.0/ProyectoAsistencia.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoAsistencia.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16298619494476d154d675786b85371b040ee4b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b79a8b6f447623146c8c5fb2980b98e3096b4f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoAsistencia.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoAsistencia.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
